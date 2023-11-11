eefc5cbf91bdbc93acdfd52068fcfc8ab8e573e3
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-11 15:42:55 +0100

    shell.h

--- shell.h
+++ shell.h
@@ -1,134 +1,96 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef CUSTOM_SHELL_H
+#define CUSTOM_SHELL_H
 
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <stdio.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
-#define END_OF_FILE -2
-#define EXIT -3
-
-/* Global environment variable */
-extern char **global_environment;
-/* Global program name */
-char *program_name;
-/* Global history counter */
-int history_count;
+#define BUFFER_SIZE 1024
+extern char **environ;
 
 /**
- * struct path_node - A new struct type defining a linked list for directories in the PATH.
- * @directory: A directory path.
- * @next: A pointer to another struct path_node.
+ * PathNode struct - a linked list to store directories in the PATH
+ * @dir_path: the directory path in the format /usr/bin
+ * @dir_length: the length of the directory string
+ * @next: pointer to the next node in the linked list
  */
-typedef struct path_node
+typedef struct PathNode
 {
-    char *directory;
-    struct path_node *next;
-} path_node_t;
+    char *dir_path;
+    unsigned int dir_length;
+    struct PathNode *next;
+} PathNode;
 
 /**
- * struct builtin_command - A new struct type defining builtin commands.
- * @name: The name of the builtin command.
- * @function: A function pointer to the builtin command's function.
+ * BuiltinCommand struct - a structure for function pointers to built-in commands
+ * @command_str: the command string (env, cd, alias, history)
+ * @fun: function pointer to the respective command function
  */
-typedef struct builtin_command
+typedef struct BuiltinCommand
 {
-    char *name;
-    int (*function)(char **arguments, char **front);
-} builtin_command_t;
+    char *command_str;
+    int (*fun)();
+} BuiltinCommand;
 
-/**
- * struct shell_alias - A new struct defining aliases.
- * @name: The name of the alias.
- * @value: The value of the alias.
- * @next: A pointer to another struct shell_alias.
- */
-typedef struct shell_alias
-{
-    char *name;
-    char *value;
-    struct shell_alias *next;
-} shell_alias_t;
+/* In builtins.c */
+int (*is_builtin(char *command))();
+int graceful_exit(char **tokens, PathNode *path_list, char *buffer);
+int display_environment(char **tokens, PathNode *environment);
+int change_directory(char **tokens);
 
-/* Global aliases linked list */
-shell_alias_t *aliases_list;
+/* In builtins_2.c */
+int set_user_environment(char **tokens);
+int handle_alias(void);
+int show_history(void);
+int custom_function(void);
 
-/* Main Helpers */
-ssize_t read_line(char **lineptr, size_t *n, FILE *stream);
-void *resize_memory(void *ptr, unsigned int old_size, unsigned int new_size);
-char **tokenize_string(char *line, char *delimiter);
-char *find_command_location(char *command);
-path_node_t *get_path_directories(char *path);
-int execute_command(char **arguments, char **front);
-void free_path_list(path_node_t *head);
-char *integer_to_string(int num);
+/* in environment.c */
+PathNode *create_path_list(void);
+PathNode *initialize_environment(void);
+char *search_command_in_path(char *cmd, PathNode *path_list);
 
-/* Input Helpers */
-void process_line(char **line, ssize_t read_size);
-void replace_variables(char **arguments, int *execution_return);
-char *parse_arguments(char *line, int *execution_return);
-int execute_arguments(char **arguments, char **front, int *execution_return);
-int run_shell(int *execution_return);
-int validate_arguments(char **arguments);
-void free_arguments(char **arguments, char **front);
-char **substitute_aliases(char **arguments);
+/* in env_operations.c */
+char *_getenv(const char *name);
+int _setenv(const char *name, const char *value, int overwrite);
 
-/* String functions */
-int string_length(const char *s);
-char *string_concatenate(char *destination, const char *source);
-char *string_concatenate_n(char *destination, const char *source, size_t n);
-char *string_copy(char *destination, const char *source);
-char *find_character(char *s, char c);
-int string_span(char *s, char *accept);
-int string_compare(char *s1, char *s2);
-int string_compare_n(const char *s1, const char *s2, size_t n);
+/* in linked_list_operations.c */
+PathNode *add_node(PathNode **head, char *path, unsigned int length);
+void free_path_list(PathNode *head);
 
-/* Builtins */
-int (*get_builtin_command(char *command))(char **arguments, char **front);
-int exit_shell(char **arguments, char **front);
-int show_environment(char **arguments, char __attribute__((__unused__)) **front);
-int set_environment(char **arguments, char __attribute__((__unused__)) **front);
-int unset_environment(char **arguments, char __attribute__((__unused__)) **front);
-int change_directory(char **arguments, char __attribute__((__unused__)) **front);
-int manage_alias(char **arguments, char __attribute__((__unused__)) **front);
-int show_help(char **arguments, char __attribute__((__unused__)) **front);
+/* In executor.c */
+void execute_command(char *argv[], PathNode *path_list);
 
-/* Builtin Helpers */
-char **copy_environment(void);
-void release_environment(void);
-char **get_environment_variable(const char *variable);
+/* In memory_management.c */
+void *_realloc_memory(char *ptr, unsigned int old_size, unsigned int new_size);
+void _memset_memory(char *str, int fill, int n);
+void _memcpy_memory(char *destination, char *source, unsigned int bytes);
 
-/* Error Handling */
-int generate_error(char **arguments, int error_code);
-char *error_message_environment(char **arguments);
-char *error_message_1(char **arguments);
-char *error_message_2_exit(char **arguments);
-char *error_message_2_cd(char **arguments);
-char *error_message_2_syntax(char **arguments);
-char *error_message_126(char **arguments);
-char *error_message_127(char **arguments);
+/* In parser.c */
+char *_read_line(int file);
+char **parse_arguments(char *str, char *delim);
+void read_input(void);
 
-/* Linkedlist Helpers */
-shell_alias_t *add_alias_end(shell_alias_t **head, char *name, char *value);
-void free_alias_list(shell_alias_t *head);
-path_node_t *add_node_end(path_node_t **head, char *directory);
-void free_path_list(path_node_t *head);
+/* In strtok.c */
+/* Functions in this file are not referenced elsewhere. */
+char *_strtok_r(char *str, char *delim, char **saveptr);
 
-void provide_help(void);
-void help_alias_command(void);
-void help_change_directory(void);
-void help_exit_command(void);
-void help_help_command(void);
-void help_show_environment(void);
-void help_set_environment(void);
-void help_unset_environment(void);
-void help_show_history(void);
+/* In string_operations.c */
+int _string_length(char *s);
+int _string_compare(char *s1, char *s2, size_t bytes);
+char *_string_duplicate(char *src);
+char *_string_concat_realloc(char *destination, char *source);
+int _convert_to_integer(char *s);
+int _is_digit(int c);
 
-int process_file_commands(char *file_path, int *execution_return);
-#endif /* SHELL_H */
+/* In string_operations_2.c */
+unsigned int count_words(char *str);
+void print_simple(const char *str);
+int _string_length_const(const char *s);
+size_t print_linked_list(const PathNode *h);
+
+#endif
