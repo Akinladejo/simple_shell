0bf9e26d1a86719e38cb3be1be45ea0cf1c7ba55
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-12 07:43:43 +0100

    shell.h

--- shell.h
+++ shell.h
@@ -1,96 +1,224 @@
 #ifndef CUSTOM_SHELL_H
 #define CUSTOM_SHELL_H
 
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
-#include <string.h>
+#include <signal.h>
+#include <limits.h>
 
 #define BUFFER_SIZE 1024
-extern char **environ;
+#define TOKEN_BUFFER_SIZE 128
+#define TOKEN_DELIMITER " \t\r\n\a"
+
+extern char **custom_environment;
 
 /**
- * PathNode struct - a linked list to store directories in the PATH
- * @dir_path: the directory path in the format /usr/bin
- * @dir_length: the length of the directory string
- * @next: pointer to the next node in the linked list
+ * struct RuntimeData - Struct holding runtime information.
+ * @arguments: Argument vector.
+ * @user_input: User-entered command line.
+ * @tokens: Tokenized command line.
+ * @execution_status: Last shell execution status.
+ * @line_counter: Counter for lines processed.
+ * @environment_variables: Array of environment variables.
+ * @process_id: Process ID of the shell.
  */
-typedef struct PathNode
-{
-    char *dir_path;
-    unsigned int dir_length;
-    struct PathNode *next;
-} PathNode;
+typedef struct RuntimeData {
+    char **arguments;
+    char *user_input;
+    char **tokens;
+    int execution_status;
+    int line_counter;
+    char **environment_variables;
+    char *process_id;
+} RuntimeData;
 
 /**
- * BuiltinCommand struct - a structure for function pointers to built-in commands
- * @command_str: the command string (env, cd, alias, history)
- * @fun: function pointer to the respective command function
+ * struct SeparatorList - Single linked list to store separators.
+ * @separator: Separator character (;, |, or &).
+ * @next: Pointer to the next node in the list.
  */
-typedef struct BuiltinCommand
-{
-    char *command_str;
-    int (*fun)();
+typedef struct SeparatorList {
+    char separator;
+    struct SeparatorList *next;
+} SeparatorList;
+
+/**
+ * struct LineList - Single linked list to store command lines.
+ * @command_line: Command line string.
+ * @next: Pointer to the next node in the list.
+ */
+typedef struct LineList {
+    char *command_line;
+    struct LineList *next;
+} LineList;
+
+/**
+ * struct RuntimeVarList - Single linked list to store variables.
+ * @variable_length: Length of the variable.
+ * @value: Value of the variable.
+ * @value_length: Length of the value.
+ * @next: Pointer to the next node in the list.
+ */
+typedef struct RuntimeVarList {
+    int variable_length;
+    char *value;
+    int value_length;
+    struct RuntimeVarList *next;
+} RuntimeVarList;
+
+/**
+ * struct BuiltinCommand - Struct for built-in command information.
+ * @name: Name of the built-in command (e.g., cd, exit, env).
+ * @function_pointer: Pointer to the function implementing the command.
+ */
+typedef struct BuiltinCommand {
+    char *name;
+    int (*function_pointer)(RuntimeData *runtime_data);
 } BuiltinCommand;
 
-/* In builtins.c */
-int (*is_builtin(char *command))();
-int graceful_exit(char **tokens, PathNode *path_list, char *buffer);
-int display_environment(char **tokens, PathNode *environment);
-int change_directory(char **tokens);
+/* Function Prototypes */
 
-/* In builtins_2.c */
-int set_user_environment(char **tokens);
-int handle_alias(void);
-int show_history(void);
-int custom_function(void);
+/* Auxiliary Lists */
+SeparatorList *add_separator_node_end(SeparatorList **head, char sep);
+void free_separator_list(SeparatorList **head);
 
-/* in environment.c */
-PathNode *create_path_list(void);
-PathNode *initialize_environment(void);
-char *search_command_in_path(char *cmd, PathNode *path_list);
+LineList *add_line_node_end(LineList **head, char *line);
+void free_line_list(LineList **head);
 
-/* in env_operations.c */
-char *_getenv(const char *name);
-int _setenv(const char *name, const char *value, int overwrite);
+RuntimeVarList *add_runtime_var_node(RuntimeVarList **head, int length_variable, char *variable, int length_value);
+void free_runtime_var_list(RuntimeVarList **head);
 
-/* in linked_list_operations.c */
-PathNode *add_node(PathNode **head, char *path, unsigned int length);
-void free_path_list(PathNode *head);
+/* Auxiliary String Functions */
+char *_custom_strcat(char *destination, const char *source);
+char *_custom_strcpy(char *destination, char *source);
+int _custom_strcmp(char *string1, char *string2);
+char *_custom_strchr(char *string, char character);
+int _custom_strspn(char *string, char *accepted_characters);
 
-/* In executor.c */
-void execute_command(char *argv[], PathNode *path_list);
+/* Auxiliary Memory */
+void _custom_memcpy(void *new_pointer, const void *pointer, unsigned int size);
+void *_custom_realloc(void *pointer, unsigned int old_size, unsigned int new_size);
+char **_custom_realloc_double_pointer(char **pointer, unsigned int old_size, unsigned int new_size);
 
-/* In memory_management.c */
-void *_realloc_memory(char *ptr, unsigned int old_size, unsigned int new_size);
-void _memset_memory(char *str, int fill, int n);
-void _memcpy_memory(char *destination, char *source, unsigned int bytes);
+/* Auxiliary String 2 */
+char *_custom_strdup(const char *string);
+int _custom_strlen(const char *string);
+int compare_chars(char string[], const char *delimiter);
+char *_custom_strtok(char string[], const char *delimiter);
+int _custom_isdigit(const char *string);
 
-/* In parser.c */
-char *_read_line(int file);
-char **parse_arguments(char *str, char *delim);
-void read_input(void);
+/* Auxiliary String 3 */
+void reverse_string(char *string);
 
-/* In strtok.c */
-/* Functions in this file are not referenced elsewhere. */
-char *_strtok_r(char *str, char *delim, char **saveptr);
+/* Check Syntax Error */
+int has_repeated_char(char *input, int index);
+int has_syntax_error_separator_operator(char *input, int index, char last);
+int is_first_char(char *input, int *index);
+void print_syntax_error(RuntimeData *runtime_data, char *input, int index, int boolean_value);
+int check_syntax_error(RuntimeData *runtime_data, char *input);
 
-/* In string_operations.c */
-int _string_length(char *s);
-int _string_compare(char *s1, char *s2, size_t bytes);
-char *_string_duplicate(char *src);
-char *_string_concat_realloc(char *destination, char *source);
-int _convert_to_integer(char *s);
-int _is_digit(int c);
+/* Shell Loop */
+char *remove_comment(char *input);
+void run_shell_loop(RuntimeData *runtime_data);
 
-/* In string_operations_2.c */
-unsigned int count_words(char *str);
-void print_simple(const char *str);
-int _string_length_const(const char *s);
-size_t print_linked_list(const PathNode *h);
+/* Read Line */
+char *read_input_line(int *end_of_file);
+ssize_t get_input_line(char **line_pointer, size_t *size, FILE *stream);
+
+/* Split */
+char *swap_characters(char *input, int boolean_value);
+void add_nodes_to_lists(SeparatorList **separator_list, LineList **line_list, char *input);
+void move_to_next(SeparatorList **separator_list, LineList **line_list, RuntimeData *runtime_data);
+int split_commands_and_execute(RuntimeData *runtime_data, char *input);
+char **split_input_line(char *input);
+
+/* Replace Variable */
+void check_environment(RuntimeVarList **head, char *input, RuntimeData *runtime_data);
+int check_variables(RuntimeVarList **head, char *input, char *string, RuntimeData *runtime_data);
+char *replace_input_with_variable(RuntimeVarList **head, char *input, char *new_input, int new_length);
+char *replace_variable(char *input, RuntimeData *runtime_data);
+
+/* Get Input Line */
+void fetch_input_line(char **line_pointer, size_t *size, char *buffer, size_t j);
+
+/* Execute Line */
+int execute_line(RuntimeData *runtime_data);
+
+/* Command Execute */
+int is_current_directory(char *path, int *index);
+char *find_executable_path(char *command, char **environment_variables);
+int is_executable_command(RuntimeData *runtime_data);
+int check_error_command(char *directory, RuntimeData *runtime_data);
+int execute_command(RuntimeData *runtime_data);
+
+/* Environment 1 */
+char *_custom_getenv(const char *name, char **environment_variables);
+int _custom_env(RuntimeData *runtime_data);
+
+/* Environment 2 */
+char *copy_information(char *name, char *value);
+void set_environment_variable(char *name, char *value, RuntimeData *runtime_data);
+int set_environment(RuntimeData *runtime_data);
+int unset_environment(RuntimeData *runtime_data);
+
+/* Change Directory */
+void change_directory_dot(RuntimeData *runtime_data);
+void change_directory_to(RuntimeData *runtime_data);
+void change_directory_previous(RuntimeData *runtime_data);
+void change_directory_to_home(RuntimeData *runtime_data);
+
+/* Change Directory Shell */
+int change_directory_shell(RuntimeData *runtime_data);
+
+/* Get Builtin Command */
+int (*get_builtin_command(char *command))(RuntimeData *runtime_data);
+
+/* Exit Custom Shell */
+int exit_custom_shell(RuntimeData *runtime_data);
+
+/* Auxiliary Stdlib */
+int get_length(int number);
+char *custom_itoa(int number);
+int custom_atoi(char *string);
+
+/* Auxiliary Error 1 */
+char *custom_strcat_change_directory(RuntimeData *, char *, char *, char *);
+char *error_get_change_directory(RuntimeData *runtime_data);
+char *error_not_found_command(RuntimeData *runtime_data);
+char *error_exit_custom_shell(RuntimeData *runtime_data);
+
+/* Auxiliary Error 2 */
+char *error_get_alias_command(char **arguments);
+char *error_environment(RuntimeData *runtime_data);
+char *error_syntax(RuntimeData *runtime_data);
+char *error_permission(RuntimeData *runtime_data);
+char *error_path_126(RuntimeData *runtime_data);
+
+/* Get Custom Shell Error */
+int get_custom_shell_error(RuntimeData *runtime_data, int evaluation);
+
+/* Get Sigint */
+void handle_sigint(int signal);
+
+/* Auxiliary Help */
+void print_help_environment(void);
+void print_help_set_environment(void);
+void print_help_unset_environment(void);
+void print_help_general(void);
+void print_help_exit(void);
+
+/* Auxiliary Help 2 */
+void print_help(void);
+void print_help_alias(void);
+void print_help_change_directory(void);
+
+/* Get Help Information */
+int get_help_information(RuntimeData *runtime_data);
 
 #endif
