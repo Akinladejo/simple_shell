23c162953620c0f5a1efd7459215de59b8474322
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-13 13:17:00 +0100

    sig_handler.c

---
+++ sig_handler.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "shell.h"
+
+/**
+ * @brief Signal handler for handling ctrl + c during runtime
+ *
+ * @param signal_number Unused parameter (just for betty)
+ */
+void handle_signal(int signal_number)
+{
+    (void)signal_number;
+    write(1, "\n$ ", 3);
+}
+
+/**
+ * @brief Signal handler for handling ctrl + c during command execution
+ *
+ * @param signal_number Unused parameter (just for betty)
+ */
+void handle_signal2(int signal_number)
+{
+    (void)signal_number;
+    write(1, "\n", 1);
+}
