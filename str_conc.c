17a48eaf67a5af7b3e9e8f532c40806074604818
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-13 13:20:38 +0100

    str_conc.c

---
+++ str_conc.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "shell.h"
+
+/**
+ * concatenate_strings - concatenates two strings
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: Pointer to the concatenated string
+ */
+char *concatenate_strings(char *s1, char *s2)
+{
+    if (s1 == NULL) {
+        s1 = "";  // Use empty string if s1 is NULL
+    }
+
+    if (s2 == NULL) {
+        s2 = "";  // Use empty string if s2 is NULL
+    }
+
+    // Calculate lengths of the strings
+    size_t l1 = string_length(s1);
+    size_t l2 = string_length(s2);
+
+    // Allocate memory for the concatenated string
+    char *result = malloc(sizeof(char) * (l1 + l2 + 1));
+
+    if (result == NULL) {
+        return NULL;  // Return NULL on malloc failure
+    }
+
+    // Copy the first string to the result
+    _string_copy(result, s1);
+
+    // Concatenate the second string to the result
+    _string_copy(result + l1, s2);
+
+    return result;
+}
