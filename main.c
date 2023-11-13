6d84934aff2f22a9df4f7f958a2a9899f9849e0a
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-13 13:11:50 +0100

    main.c

--- main.c
+++ main.c
@@ -1,136 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
 #include "shell.h"
-/*
- * File: shell.c
- * Author: Akinladejo Andrew and Osei Kofi
- */
-
-void signalHandler(int sig);
-int executeCommand(char **args, char **front);
 
 /**
- * signalHandler - Handles signals and prints a new prompt.
- * @sig: The signal number.
- */
-void signalHandler(int sig)
-{
-	char *newPrompt = "\n$ ";
-	
-	(void)sig;
-	signal(SIGINT, signalHandler);
-	write(STDIN_FILENO, newPrompt, 3);
-}
-
-/**
- * executeCommand - Execute a command in a child process.
- * @args: An array of command arguments.
- * @front: A pointer to the beginning of the args array.
+ * @brief Main function for the shell program
  *
- * Return: If an error occurs - a corresponding error code.
- *         Otherwise - The exit value of the last executed command.
+ * @param argc Count of input parameters to program
+ * @param argv Parameters as input to program
+ * @param env Unix environment
+ *
+ * @return Should only exit through shell inputs
  */
-int executeCommand(char **args, char **front)
-{
-	pid_t childPid;
-	int status, flag = 0, ret = 0;
-	char *command = args[0];
-	
-	if (command[0] != '/' && command[0] != '.')
-    {
-		flag = 1;
-		command = getCommandLocation(command);
-	}
-	if (!command || (access(command, F_OK) == -1))
-	{
-		if (errno == EACCES)
-			ret = (createError(args, 126));
-		else
-			ret = (createError(args, 127));
-	}
-	else
-	{
-		childPid = fork();
-		if (childPid == -1)
-		{
-			if (flag)
-				free(command);
-			perror("Error child:");
-			return (1);
-		}
-		if (childPid == 0)
-		{
-			execve(command, args, environment);
-			if (errno == EACCES)
-				ret = (createError(args, 126));
-			freeEnvironment();
-			freeArguments(args, front);
-			freeAliasList(aliases);
-			_exit(ret);
-		}
-		else
-		{
-			wait(&status);
-			ret = WEXITSTATUS(status);
-		}
-	}
-	if (flag)
-		free(command);
-	return (ret);
+int main(int argc, char **argv, char **env) {
+    size_t buffer_size = 0;
+    char **command, *path_command, *input_buffer = NULL;
+    ShellInfo *shell_info;
+    int error_number = 0, exit_number = 0, logical_relation = 0, is_runnable = 0, size_env, unset_environment = 0;
+    ssize_t is_builtin;
+
+    // Check for valid input
+    if (argc > 1 || argv == NULL) {
+        write(2, "Please run with no arguments\n", 29);
+        exit(127);
+    }
+
+    // Set up signal handler
+    signal(SIGINT, handle_signal);
+
+    // Initialize shell information structure
+    size_env = string_array_length(env);
+    env = copy_double_pointer(env, size_env, size_env);
+    shell_info = initialize_shell_info(argv[0], &error_number, &exit_number, &logical_relation, &is_runnable, &env, &unset_environment);
+
+    // Main shell loop
+    while (1) {
+        command = NULL;
+        command = check_input(argc, argv, &buffer_size, &input_buffer, shell_info);
+
+        if (!command) {
+            continue;
+        }
+
+        add_command(shell_info, input_buffer, command[0], command);
+        is_builtin = execute_built_ins(shell_info);
+
+        switch (is_builtin) {
+            case -1:
+            case 1:
+                continue;
+            default:
+                break;
+        }
+
+        path_command = _get_absolute_path(command[0], env, shell_info);
+        add_path_to_command(shell_info, path_command);
+
+        if (!path_command) {
+            free_double_pointer(command);
+            shell_info->error_count[0] += 1;
+            handle_error(0, shell_info, 127);
+        } else if (access(path_command, X_OK) == -1) {
+            handle_error(1, shell_info, 126);
+        } else {
+            execute_command(path_command, command, env, shell_info);
+        }
+
+        free_double_pointer(command);
+        free(path_command);
+    }
+
+    // Cleanup
+    free_double_pointer(*(shell_info->environment_copy));
+    free(shell_info);
+    return 0;
 }
 
-/**
- * main - Entry point for a simple UNIX command interpreter.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: The return value of the last executed command.
- */
-int main(int argc, char *argv[])
-{
-	int ret = 0, retn;
-	int *executionResult = &retn;
-	char *prompt = "$ ", *newLine = "\n";
-	
-	programName = argv[0];
-	history = 1;
-	aliases = NULL;
-	signal(SIGINT, signalHandler);
-	
-	*executionResult = 0;
-	environment = copyEnvironment();
-	if (!environment)
-		exit(-100);
-	
-	if (argc != 1)
-	{
-		ret = processFileCommands(argv[1], executionResult);
-		freeEnvironment();
-		freeAliasList(aliases);
-		return (*executionResult);
-	}
-	if (!isatty(STDIN_FILENO))
-	{
-		while (ret != END_OF_FILE && ret != EXIT)
-			ret = handleArguments(executionResult);
-		freeEnvironment();
-		freeAliasList(aliases);
-		return (*executionResult);
-	}
-	
-	while (1)
-	{
-		write(STDOUT_FILENO, prompt, 2);
-		ret = handleArguments(executionResult);
-		if (ret == END_OF_FILE || ret == EXIT)
-		{
-			if (ret == END_OF_FILE)
-				write(STDOUT_FILENO, newLine, 1);
-			freeEnvironment();
-			freeAliasList(aliases);
-			exit(*executionResult);
-		}
-	}
-	
-	freeEnvironment();
-	freeAliasList(aliases);
-	return (*executionResult);
-}
+// Rest of the code remains unchanged
