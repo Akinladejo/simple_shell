6e4c8ccaf420366c133839857995c41d5181c52a
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-15 04:25:09 +0100

    Update dup_str.c

--- dup_str.c
+++ dup_str.c
@@ -8,20 +8,21 @@
  * Return: Pointer to the duplicated string
  * On error: NULL for inappropriate entry or memory allocation failure
  */
-char *_str_duplicate(char *str) {
+char *_str_duplicate(char *str)
+{
 	char *duplicate;
 
 	if (!str)
-		return NULL;
+		return (NULL);
 
 	duplicate = malloc((_strlen(str) * sizeof(*duplicate)) + 1);
 
 	if (!duplicate)
-		return NULL;
+		return (NULL);
 
 	_strcpy(duplicate, str);
 
-	return duplicate;
+	return (duplicate);
 }
 
 /**
@@ -31,24 +32,27 @@ char *_str_duplicate(char *str) {
  * Return: Length of the string
  * On error: -1 for inappropriate entry
  */
-int string_length(char *s) { return ((!*s) ? 0 : 1 + string_length(s + 1)); }
+int string_length(char *s)
+{
+	return (!*s) ? 0 : 1 + string_length(s + 1);
+}
 
 /**
- * _strcpy - Copy a source string to a destination string
- * @dest: Destination where the string will be stored
- * @src: Source string to copy from
+ * _string_copy - Copy a source string to a destination string
+ * @destination: Destination where the string will be stored
+ * @source: Source string to copy from
  *
  * Return: Pointer to the destination string
  * On error: NULL for inappropriate entry
  */
-char *_string_copy(char *destination, char *source);
+char *_string_copy(char *destination, char *source)
 {
 	int i = 0;
 
 	for (i = 0; *(source + i) != '\0'; i++)
-		*(destination + i) = *(src + i);
+		*(destination + i) = *(source + i);
 
-	*(destestination + i) = *(source + i); /* Adding '\0' character */
+	*(destination + i) = *(source + i); /* Adding '\0' character */
 
-	return destination;
+	return (destination);
 }
