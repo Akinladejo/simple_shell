1a47ac74aece1640f987ad385dcda30b4f26a5fa
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-15 12:41:11 +0100

    Update get_env.c

--- get_env.c
+++ get_env.c
@@ -12,24 +12,31 @@ char *_get_environment_variable(const char *name, char **env)
     if (!env || !name)
         return (NULL);
 
+    /* Calculate the length of the name */
     size_t name_length = 0;
     while (name[name_length] != '\0')
         name_length++;
 
     size_t check;
+    size_t j;
+
+    /* Iterate through the environment variables */
     for (int i = 0; env[i] != NULL; i++) {
         check = 0;
-        size_t j = 0;
+        j = 0;
 
+        /* Compare characters in the name with the current environment variable */
         while (j < name_length && env[i][j] != '\0') {
             if (env[i][j] == name[j])
                 check++;
             j++;
         }
 
+        /* Check if the full name is matched, and the next character is '=' */
         if (check == name_length && env[i][check] == '=')
             return (env[i]);
     }
 
+    /* Return NULL if the environment variable is not found */
     return (NULL);
 }
