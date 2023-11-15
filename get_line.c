85299fd12b77f5c7df40ea0d499f2d5e0d098bd7
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-15 04:46:08 +0100

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -9,13 +9,16 @@
 /**
  * _memset - Fills the first n bytes of the memory area pointed to by dest
  *           with the constant byte value.
+ *
  * @dest: Pointer to the memory area
  * @value: Byte value to be set
  * @count: Number of bytes to set
  * Return: Pointer to the memory area dest
  */
-char *_memset(char *dest, char value, unsigned int count) {
-	for (unsigned int i = 0; i < count; i++) {
+char *_memset(char *dest, char value, unsigned int count)
+{
+	for (unsigned int i = 0; i < count; i++)
+	{
 		dest[i] = value;
 	}
 	return dest;
@@ -23,88 +26,101 @@ char *_memset(char *dest, char value, unsigned int count) {
 
 /**
  * _realloc - Reallocates a memory block using malloc and free
+ *
  * @ptr: Pointer to the old memory block
  * @old_size: Size of the old memory block
  * @new_size: Size of the new memory block
- * Return: Pointer to the newly allocated memory block
+ * Return: (Pointer to the newly allocated memory block)
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
 	void *new_ptr = malloc(new_size);
-	if (new_ptr == NULL) {
-		return NULL;
+	if (new_ptr == NULL)
+	{
+		return (NULL);
 	}
 
-	if (ptr != NULL && old_size > 0) {
+	if (ptr != NULL && old_size > 0)
+	{
 		memcpy(new_ptr, ptr, old_size);
 		free(ptr);
 	}
 
-	return new_ptr;
+	return (new_ptr);
 }
 
 /**
  * get_line - Reads a line from a file descriptor into a buffer
+ *
  * @buffer: Pointer to the buffer that will contain the line
  * @buffer_size: Pointer to the size of the buffer
  * @file_descriptor: File descriptor to read from
- * Return: Number of bytes read, or -1 on failure
+ * Return: (Number of bytes read, or -1 on failure)
  */
-int get_line(char **buffer, size_t *buffer_size, int file_descriptor) {
+int get_line(char **buffer, size_t *buffer_size, int file_descriptor)
+{
 	char *buf = NULL;
 	size_t size = BUFFER_SIZE;
 	size_t len = 0;
 	int read_result;
 
 	buf = malloc(size);
-	if (buf == NULL) {
-		return -1;
+	if (buf == NULL)
+	{
+		return (-1);
 	}
 
-	while (1) {
+	while (1)
+	{
 		read_result = read(file_descriptor, buf + len, size - len);
-		if (read_result == -1) {
+		if (read_result == -1)
+		{
 			free(buf);
-			return -1;
+			return (-1);
 		}
 
-		if (read_result == 0) {
+		if (read_result == 0)
+		{
 			break;
 		}
 
 		len += read_result;
 
-		if (len >= size) {
+		if (len >= size)
+		{
 			size *= 2;
 			buf = _realloc(buf, size, size);
-			if (buf == NULL) {
+			if (buf == NULL)
+			{
 				free(buf);
-				return -1;
+				return (-1);
 			}
 		}
 
-		for (size_t i = 0; i < len; i++) {
-			switch (buf[i]) {
+		for (size_t i = 0; i < len; i++)
+		{
+			switch (buf[i])
+			{
 			case '\n':
 				buf[i] = '\0';
 				*buffer = buf;
 				*buffer_size = size;
-				return len;
+				return (len);
 
 			default:
-				// Additional cases can be added here for
-				// different characters
 				break;
 			}
 		}
 	}
 
-	if (len == 0) {
+	if (len == 0)
+	{
 		free(buf);
-		return -1;
+		return (-1);
 	}
 
 	buf[len] = '\0';
 	*buffer = buf;
 	*buffer_size = size;
-	return len;
+	return (len);
 }
