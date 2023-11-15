dae38c5681e66cc89f546c3581c8ee781807dd95
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-15 09:08:42 +0100

    Update builtin2.c

--- builtin2.c
+++ builtin2.c
@@ -65,69 +65,109 @@ char *auxiliary_cd(ShellInfo *shell_info, char *current_directory)
  */
 ssize_t cd_command(ShellInfo *shell_info)
 {
-	char *current_directory = NULL, *directory = NULL, **new_environment,
-	     *oldpwd = NULL;
-	int exit_code = 1, check = 1, check_minus = 0;
+    char *current_directory = getcwd(NULL, 4096);
+    if (!current_directory)
+    {
+        handle_error(4, shell_info, 2);
+        free(shell_info->command_options);
+        return (-1);
+    }
 
-	current_directory = getcwd(NULL, 4096);
-	if (!current_directory)
-		return ((handle_error(4, shell_info, 2),
-		       free(shell_info->command_options), -1);
+    char *directory = determine_directory(shell_info, current_directory);
+    if (!directory)
+    {
+        free(shell_info->command_options);
+        free(current_directory);
+        return (1);
+    }
 
-	switch (!shell_info->command_options[1] ||
-		(!_compare_strings(shell_info->command_options[1], "~")))
-	{
-	case 1:
-		directory = auxiliary_cd_2(shell_info, current_directory);
-		if (!directory)
-			return (free(shell_info->command_options),
-			       free(current_directory), 1);
-		break;
+    int result = change_directory(directory, shell_info);
 
-	default:
-		if (!_compare_strings(shell_info->command_options[1], "-"))
-		{
-			directory = auxiliary_cd(shell_info, current_directory);
-			if (!directory)
-				return (free(shell_info->command_options),
-				       free(current_directory), 1);
-			check_minus = 1;
-		}
-		else
-		{
-			directory = shell_info->command_options[1];
-		}
-	}
+    if (result == 0 && shell_info->check_minus == 1)
+    {
+        write(1, directory, _string_length(directory));
+        write(1, "\n", 1);
+    }
 
-	if (directory)
-		check = chdir(directory);
+    handle_directory_change_result(result, shell_info, current_directory, directory);
 
-	if (check == 0 && check_minus == 1)
-		write(1, directory, _string_length(directory)),
-		    write(1, "\n", 1);
+    free_resources(shell_info, current_directory, directory);
 
-	switch (check != 0)
-	{
-	case 1:
-		handle_error(4, shell_info, 2);
-		exit_code = -1;
-		break;
-	default:
-		new_environment = set_environment_variable(*(shell_info->environment_copy), "PWD", directory, shell_info);
-		*(shell_info->environment_copy) = new_environment;
+    return (shell_info->exit_code);
+}
 
-		new_environment = set_environment_variable(
-		    *(shell_info->environment_copy), "OLDPWD",
-		    current_directory, shell_info);
-		*(shell_info->environment_copy) = new_environment;
-	}
+char *determine_directory(ShellInfo *shell_info, const char *current_directory)
+{
+    char *directory = NULL;
 
-	free(shell_info->command_options);
-	free(current_directory);
-	free(oldpwd);
+    if (!shell_info->command_options[1] || !_compare_strings(shell_info->command_options[1], "~"))
+    {
+        directory = auxiliary_cd_2(shell_info, current_directory);
+    }
+    else
+    {
+        if (!_compare_strings(shell_info->command_options[1], "-"))
+        {
+            directory = auxiliary_cd(shell_info, current_directory);
+            shell_info->check_minus = 1;
+        }
+        else
+        {
+            directory = shell_info->command_options[1];
+        }
+    }
 
-	if (check_minus == 1)
-		free(directory);
+    return (directory);
+}
 
-	return (exit_code);
+int change_directory(const char *directory, ShellInfo *shell_info)
+{
+    int result = chdir(directory);
+    if (result != 0)
+    {
+        handle_error(4, shell_info, 2);
+        shell_info->exit_code = -1;
+    }
+    else
+    {
+        update_environment_variables(shell_info, directory);
+    }
+
+    return (result);
+}
+
+void update_environment_variables(ShellInfo *shell_info, const char *directory)
+{
+    char **new_environment = set_environment_variable(*(shell_info->environment_copy), "PWD", directory, shell_info);
+    *(shell_info->environment_copy) = new_environment;
+
+    new_environment = set_environment_variable(*(shell_info->environment_copy), "OLDPWD", shell_info->current_directory, shell_info);
+    *(shell_info->environment_copy) = new_environment;
+
+    shell_info->current_directory = directory;
+}
+
+void handle_directory_change_result(int result, ShellInfo *shell_info, const char *current_directory, const char *directory)
+{
+    if (result != 0)
+    {
+        handle_error(4, shell_info, 2);
+        shell_info->exit_code = -1;
+    }
+    else
+    {
+        update_environment_variables(shell_info, directory);
+    }
+}
+
+void free_resources(ShellInfo *shell_info, char *current_directory, char *directory)
+{
+    free(shell_info->command_options);
+    free(current_directory);
+    free(shell_info->oldpwd);
+
+    if (shell_info->check_minus == 1)
+    {
+        free(directory);
+    }
 }
