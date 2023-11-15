daeb625b223f0d88beab446861b0608562e5253c
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-15 10:18:59 +0100

    Update builtin3.c

--- builtin3.c
+++ builtin3.c
@@ -6,100 +6,100 @@
 #include <unistd.h>
 
 /**
- * power - Calculate the power of a number
- * @base: base number
- * @exponent: power
+ * power - Calculate the power of a number.
+ * @base: base number.
+ * @exponent: power.
  *
- * Return: Result of base to the power of exponent
+ * Return: Result of base to the power of exponent.
  */
 long power(long base, long exponent)
 {
-	long result = 1;
+    long result = 1;
 
-	for (long i = 0; i < exponent; i++)
-	{
-		result *= base;
-	}
+    for (long i = 0; i < exponent; i++)
+    {
+        result *= base;
+    }
 
-	return (result);
+    return (result);
 }
 
 /**
- * convert_to_integer - Convert a string to an integer
- * @s: input string
+ * convert_to_integer - Convert a string to an integer.
+ * @s: input string.
  *
- * Return: Transformed integer On error: -1 for inappropriate entry
+ * Return: Transformed integer. On error: (-1) for inappropriate entry.
  */
 long convert_to_integer(char *s)
 {
-	if (!s)
-	{
-		return (0);
-	}
+    if (!s)
+    {
+        return (0);
+    }
 
-	long result = 0;
-	long sign = 1;
+    long result = 0;
+    long sign = 1;
 
-		if (*s == '-')
-	{
-		sign = -1;
-		s++;
-	}
+    if (*s == '-')
+    {
+        sign = -1;
+        s++;
+    }
 
-		while (*s >= '0' && *s <= '9')
-	{
-		result = result * 10 + (*s - '0');
-		s++;
-	}
+    while (*s >= '0' && *s <= '9')
+    {
+        result = result * 10 + (*s - '0');
+        s++;
+    }
 
-	return (sign * result);
+    return (sign * result);
 }
 
 /**
- * compare_strings - Compare two strings
- * @string1: first string
- * @string2: second string
+ * compare_strings - Compare two strings.
+ * @string1: first string.
+ * @string2: second string.
  *
- * Return: 0 if strings are equal, another value if not
+ * Return: 0 if strings are equal, another value if not.
  */
 int compare_strings(char *string1, char *string2)
 {
-	while (*string1 && *string1 == *string2)
-	{
-		string1++;
-		string2++;
-	}
+    while (*string1 && *string1 == *string2)
+    {
+        string1++;
+        string2++;
+    }
 
-	return (*string1 - *string2);
+    return (*string1 - *string2);
 }
 
 /**
- * is_digit - Check if a character is a digit
- * @character: character to check
+ * is_digit - Check if a character is a digit.
+ * @character: character to check.
  *
- * Return: 1 if digit, 0 if not
+ * Return: 1 if digit, 0 if not.
  */
 int is_digit(int character)
 {
-	return (character >= '0' && character <= '9');
+    return (character >= '0' && character <= '9');
 }
 
 /**
- * is_number - Check if a string is a number
- * @string: input string
+ * is_number - Check if a string is a number.
+ * @string: input string.
  *
- * Return: 1 if the string has only numbers, 0 if not
+ * Return: 1 if the string has only numbers, 0 if not.
  */
 int is_number(char *string)
 {
-	while (*string)
-	{
-		if (!is_digit(*string))
-		{
-			return (0);
-		}
-		string++;
-	}
+    while (*string)
+    {
+        if (!is_digit(*string))
+        {
+            return (0);
+        }
+        string++;
+    }
 
-	return (1);
+    return (1);
 }
