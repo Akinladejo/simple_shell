d9d45a8793bc3fcfbb68145e1ff5cedf8dcb9fcc
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-15 11:12:11 +0100

    Update set_env.c

--- set_env.c
+++ set_env.c
@@ -5,17 +5,20 @@
 /**
  * free_double_pointer - Frees a double pointer.
  * @pointer: The double pointer to be freed.
- * Return: free dbp
+ *
+ * This function frees the memory occupied by a double pointer.
+ * Return: None
  */
 void free_double_pointer(char **pointer)
 {
-	if (!pointer)
-		return;
+    if (!pointer)
+        return;
 
-	for (int i = 0; pointer[i] != NULL; i++)
-		free(pointer[i]);
+    int i;
+    for (i = 0; pointer[i] != NULL; i++)
+        free(pointer[i]);
 
-	free(pointer);
+    free(pointer);
 }
 
 /**
@@ -23,56 +26,56 @@ void free_double_pointer(char **pointer)
  * @pointer: The original double pointer.
  * @old_size: The old size of the double pointer.
  * @new_size: The new size of the double pointer.
- * Return: The copied double pointer or (NULL) on failure.
+ *
+ * This function copies a double pointer to a new size.
+ * Returns the copied double pointer or NULL on failure.
  */
 char **copy_double_pointer(char **pointer, int old_size, int new_size)
 {
-	switch (old_size == new_size)
-	{
-	case 1:
-		return (NULL);
-	}
+    if (old_size == new_size)
+        return (NULL);
 
-	int copy_size = (new_size < old_size) ? new_size : old_size;
-	char **copy = malloc(sizeof(char *) * (copy_size + 1));
+    int copy_size = (new_size < old_size) ? new_size : old_size;
+    char **copy = malloc(sizeof(char *) * (copy_size + 1));
 
-	switch (!!pointer)
-	{
-	case 1:
-		for (int i = 0; i < copy_size; i++)
-		{
-			copy[i] = _str_duplicate(pointer[i]);
+    if (!copy)
+        return (NULL);
 
-			switch (!copy[i])
-			{
-			case 1:
-				while (--i >= 0)
-					free(copy[i]);
-				free(copy);
-				return (NULL);
-			}
-		}
-	}
+    int i;
+    for (i = 0; i < copy_size; i++)
+    {
+        copy[i] = _str_duplicate(pointer[i]);
 
-	copy[new_size] = NULL;
-	return (copy);
+        if (!copy[i])
+        {
+            while (--i >= 0)
+                free(copy[i]);
+            free(copy);
+            return (NULL);
+        }
+    }
+
+    copy[new_size] = NULL;
+    return (copy);
 }
 
 /**
  * string_array_length - Calculates the length of a string array.
  * @array: The string array.
+ *
+ * This function calculates the length of a string array.
  * Return: The length of the string array.
  */
 int string_array_length(char **array)
 {
-	if (!array)
-		return (0);
+    if (array == NULL)
+        return (0);
 
-	int length;
-	for (length = 0; array[length] != NULL; length++)
-		;
+    int length;
+    for (length = 0; array[length] != NULL; length++)
+        ;
 
-	return (length);
+    return (length);
 }
 
 /**
@@ -81,77 +84,67 @@ int string_array_length(char **array)
  * @variable: The variable to be set.
  * @value: The value to set for the variable.
  * @shell_info: The shell information.
- * Return: The modified environment array or (NULL) on failure.
+ *
+ * This function sets an environment variable.
+ * Returns the modified environment array or NULL on failure.
  */
 char **set_environment_variable(char **env, char *variable, char *value, ShellInfo *shell_info)
 {
-	if (!variable || string_length(variable) == 0)
-		return (handle_error(3, shell_info, 1), NULL);
+    if (!variable || string_length(variable) == 0)
+        return (handle_error(3, shell_info, 1), NULL);
 
-	char *env_join2 = concatenate_strings(variable, "=");
-	switch (!!env_join2) {
-	case 0:
-		return (handle_error(3, shell_info, 1), NULL);
-	}
+    char *env_join2 = concatenate_strings(variable, "=");
+    if (!env_join2)
+        return (handle_error(3, shell_info, 1), NULL);
 
-	char *env_join = concatenate_strings(env_join2, value);
-	free(env_join2);
+    char *env_join = concatenate_strings(env_join2, value);
+    free(env_join2);
 
-	switch (!!env_join)
-	{
-	case 0:
-		return (handle_error(3, shell_info, 1), NULL);
-	}
+    if (!env_join)
+        return (handle_error(3, shell_info, 1), NULL);
 
-	int len_variable = string_length(variable);
-	int len_env = string_array_length(env);
+    int len_variable = string_length(variable);
+    int len_env = string_array_length(env);
 
-	for (int i = 0; env && env[i] != NULL; i++)
-	{
-		int check = 0;
-		for (int j = 0; j < len_variable && env[i][j] != NULL; j++)
-		{
-			if (variable[j] == '=')
-				return (free(env_join), handle_error(3, shell_info, 2), NULL);
+    int i;
+    for (i = 0; env && env[i] != NULL; i++)
+    {
+        int check = 0;
+        int j;
+        for (j = 0; j < len_variable && env[i][j] != NULL; j++)
+        {
+            if (variable[j] == '=')
+                return (free(env_join), handle_error(3, shell_info, 2), NULL);
 
-			if (env[i][j] == variable[j])
-				check++;
-		}
+            if (env[i][j] == variable[j])
+                check++;
+        }
 
-		if (check == len_variable && env[i][check] == '=')
-		{
-			free(env[i]);
-			char *copy_dup = _str_duplicate(env_join);
-			free(env_join);
+        if (check == len_variable && env[i][check] == '=')
+        {
+            free(env[i]);
+            char *copy_dup = _str_duplicate(env_join);
+            free(env_join);
 
-			switch (!copy_dup)
-			{
-			case 1:
-				return (handle_error(3, shell_info, 1), NULL);
-			}
+            if (!copy_dup)
+                return (handle_error(3, shell_info, 1), NULL);
 
-			return (env[i] = copy_dup, env);
-		}
-	}
+            return (env[i] = copy_dup, env);
+        }
+    }
 
-	char **copy = copy_double_pointer(env, len_env, len_env + 1);
-	free_double_pointer(env);
+    char **copy = copy_double_pointer(env, len_env, len_env + 1);
+    free_double_pointer(env);
 
-	switch (!copy)
-	{
-	case 1:
-		return (free(env_join), handle_error(3, shell_info, 1), NULL);
-	}
+    if (!copy)
+        return (free(env_join), handle_error(3, shell_info, 1), NULL);
 
-	env = copy;
-	char *copy_dup = _str_duplicate(env_join);
-	free(env_join);
+    env = copy;
+    char *copy_dup = _str_duplicate(env_join);
+    free(env_join);
 
-	switch (!copy_dup)
-	{
-	case 1:
-		return (handle_error(3, shell_info, 1), NULL);
-	}
+    if (!copy_dup)
+        return (handle_error(3, shell_info, 1), NULL);
 
-	return (env[len_env] = copy_dup, env;)
+    return (env[len_env] = copy_dup, env);
 }
