744683f90bbe4f99f20434448758218fc8d1c3a5
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-15 11:15:59 +0100

    Update str_conc.c

--- str_conc.c
+++ str_conc.c
@@ -10,29 +10,31 @@
  */
 char *concatenate_strings(const char *s1, const char *s2)
 {
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
+    size_t l1, l2;
+    
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
 
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
+    if (s2 == NULL)
+    {
+        s2 = "";
+    }
 
-	size_t l1 = string_length(s1);
-	size_t l2 = string_length(s2);
+    l1 = string_length(s1);
+    l2 = string_length(s2);
 
-	char *result = malloc(sizeof(char) * (l1 + l2 + 1));
+    char *result = malloc(sizeof(char) * (l1 + l2 + 1));
 
-	if (result == NULL)
-	{
-		return (NULL);
-	}
+    if (result == NULL)
+    {
+        return (NULL);
+    }
 
-	_string_copy(result, s1);
+    _string_copy(result, s1);
 
-	_string_copy(result + l1, s2);
+    _string_copy(result + l1, s2);
 
-	return (result);
+    return (result);
 }
