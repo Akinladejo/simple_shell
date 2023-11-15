a6e0606ccb440228902eaab56ff89d9c96b41f6b
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-15 10:42:33 +0100

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -13,15 +13,16 @@
  * @dest: Pointer to the memory area
  * @value: Byte value to be set
  * @count: Number of bytes to set
- * Return: Pointer to the memory area dest
+ * Return: (Pointer to the memory area dest)
  */
 char *_memset(char *dest, char value, unsigned int count)
 {
-	for (unsigned int i = 0; i < count; i++)
-	{
-		dest[i] = value;
-	}
-	return (dest);
+    unsigned int i;  /* Declare 'i' outside of the loop */
+    for (i = 0; i < count; i++)
+    {
+        dest[i] = value;
+    }
+    return (dest);
 }
 
 /**
@@ -34,19 +35,19 @@ char *_memset(char *dest, char value, unsigned int count)
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-	{
-		return (NULL);
-	}
+    void *new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
+    {
+        return (NULL);
+    }
 
-	if (ptr != NULL && old_size > 0)
-	{
-		memcpy(new_ptr, ptr, old_size);
-		free(ptr);
-	}
+    if (ptr != NULL && old_size > 0)
+    {
+        memcpy(new_ptr, ptr, old_size);
+        free(ptr);
+    }
 
-	return (new_ptr);
+    return (new_ptr);
 }
 
 /**
@@ -59,68 +60,69 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
  */
 int get_line(char **buffer, size_t *buffer_size, int file_descriptor)
 {
-	char *buf = NULL;
-	size_t size = BUFFER_SIZE;
-	size_t len = 0;
-	int read_result;
+    char *buf = NULL;
+    size_t size = BUFFER_SIZE;
+    size_t len = 0;
+    int read_result;
 
-	buf = malloc(size);
-	if (buf == NULL)
-	{
-		return (-1);
-	}
+    buf = malloc(size);
+    if (buf == NULL)
+    {
+        return (-1);
+    }
 
-	while (1)
-	{
-		read_result = read(file_descriptor, buf + len, size - len);
-		if (read_result == -1)
-		{
-			free(buf);
-			return (-1);
-		}
+    size_t i;  /* Declare 'i' outside of the loop */
+    while (1)
+    {
+        read_result = read(file_descriptor, buf + len, size - len);
+        if (read_result == -1)
+        {
+            free(buf);
+            return (-1);
+        }
 
-		if (read_result == 0)
-		{
-			break;
-		}
+        if (read_result == 0)
+        {
+            break;
+        }
 
-		len += read_result;
+        len += read_result;
 
-		if (len >= size)
-		{
-			size *= 2;
-			buf = _realloc(buf, size, size);
-			if (buf == NULL)
-			{
-				free(buf);
-				return (-1);
-			}
-		}
+        if (len >= size)
+        {
+            size *= 2;
+            buf = _realloc(buf, size, size);
+            if (buf == NULL)
+            {
+                free(buf);
+                return (-1);
+            }
+        }
 
-		for (size_t i = 0; i < len; i++)
-		{
-			switch (buf[i])
-			{
-			case '\n':
-				buf[i] = '\0';
-				*buffer = buf;
-				*buffer_size = size;
-				return (len);
+        for (i = 0; i < len; i++)
+        {
+            switch (buf[i])
+            {
+            case '\n':
+                buf[i] = '\0';
+                *buffer = buf;
+                *buffer_size = size;
+                return (len);
 
-			default:
-				break;
-			}
-		}
-	}
+            default:
+                break;
+            }
+        }
+    }
 
-	if (len == 0)
-	{
-		free(buf);
-		return (-1);
-	}
+    if (len == 0)
+    {
+        free(buf);
+        return (-1);
+    }
 
-	buf[len] = '\0';
-	*buffer = buf;
-	*buffer_size = size;
-	return (len);
+    buf[len] = '\0';
+    *buffer = buf;
+    *buffer_size = size;
+    return (len);
 }
