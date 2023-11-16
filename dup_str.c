e90b6b6a84ef001cb80150dbb1d2f30fa039f117
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-16 22:58:01 +0100

    Update dup_str.c

--- dup_str.c
+++ dup_str.c
@@ -32,7 +32,7 @@ char *_str_duplicate(const char *str)
  */
 int string_length(const char *s)
 {
-	return (!*s) ? 0 : 1 + string_length(s + 1);
+	return ((!*s) ? 0 : 1 + string_length(s + 1));
 }
 
 /**
