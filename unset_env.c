dfa9633b2fabd4a1b34063b1a614a578383a7d4f
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-16 22:31:52 +0100

    Update unset_env.c

--- unset_env.c
+++ unset_env.c
@@ -12,31 +12,36 @@
  *
  * Return: Pointer malloec
  */
-char **_copy_double_pointer(char **p, int new_size, int jump) {
-    char **copy;
-    int i, j, csize;
+char **_copy_double_pointer(char **p, int new_size, int jump)
+{
+	char **copy;
+	int i, j, csize;
 
-    csize = new_size;
-    copy = malloc(sizeof(char *) * (csize + 1));
-    if (copy == NULL)
-	return (NULL);
+	csize = new_size;
+	copy = malloc(sizeof(char *) * (csize + 1));
 
-    for (i = 0, j = 0; j < csize; i++, j++) {
-	if (i == jump)
-	    i++;
-	copy[j] = _str_duplicate(p[i]);
-	if (copy[j] == NULL) {
-	    j--;
-	    for (; j >= 0; j--)
-		free(copy[j]);
-	    free(copy);
-	    return (NULL);
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0, j = 0; j < csize; i++, j++)
+	{
+		if (i == jump)
+			i++;
+		copy[j] = _str_duplicate(p[i]);
+		if (copy[j] == NULL)
+		{
+			j--;
+
+			for (; j >= 0; j--)
+				free(copy[j]);
+			free(copy);
+
+			return (NULL);
+		}
 	}
-    }
 
-    copy[new_size] = NULL;
-
-    return (copy);
+	copy[new_size] = NULL;
+	return (copy);
 }
 
 /**
@@ -49,60 +54,71 @@ char **_copy_double_pointer(char **p, int new_size, int jump) {
  * Return: Modified environment array on success, NULL on error
  */
 char **_unset_environment_variable(char **env, const char *variable,
-				   ShellInfo *shell_info) {
-    int i, j, check, l = 0, lenv = 0, found = 0;
-    char **copy;
+ShellInfo *shell_info)
+{
+	int i, j, check, l = 0, lenv = 0, found = 0;
+	char **copy;
 
-    shell_info->unset_environment[0] = 0;
+	shell_info->unset_environment[0] = 0;
 
-    if (!env) {
-	handle_error(3, shell_info, 1);
-	return (NULL);
-    }
-
-    if (string_length(variable) == 0 || variable == NULL) {
-	handle_error(3, shell_info, 2);
-	return (NULL);
-    }
-
-    l = string_length(variable);
-    lenv = string_array_length(env);
-
-    for (i = 0; env[i] != NULL; i++) {
-	for (check = 0, j = 0; j < l && env[i][j] != '\0'; j++) {
-	    if (variable[j] == '=') {
-		handle_error(3, shell_info, 3);
+	if (!env)
+	{
+		handle_error(3, shell_info, 1);
 		return (NULL);
-	    }
-
-	    if (env[i][j] == variable[j])
-		check++;
 	}
 
-	if (check == l && env[i][check] == '=') {
-	    /* Found env to erase */
-	    found = 1;
+	if (string_length(variable) == 0 || variable == NULL)
+	{
+		handle_error(3, shell_info, 2);
+		return (NULL);
+	}
 
-	    if ((lenv - 1) != 0) {
-		copy = _copy_double_pointer(env, lenv - 1, i);
-		if (copy == NULL) {
-		    handle_error(7, shell_info, 1);
-		    return (NULL);
+	l = string_length(variable);
+	lenv = string_array_length(env);
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		for (check = 0, j = 0; j < l && env[i][j] != '\0'; j++)
+		{
+			if (variable[j] == '=')
+			{
+				handle_error(3, shell_info, 3);
+				return (NULL);
+			}
+
+			if (env[i][j] == variable[j])
+				check++;
 		}
-	    } else {
-		shell_info->unset_environment[0] = 1;
-		copy = NULL;
-	    }
 
-	    free_double_pointer(env);
-	    return (copy);
+		if (check == l && env[i][check] == '=')
+		{
+			/* Found env to erase */
+			found = 1;
+
+			if ((lenv - 1) != 0)
+			{
+				copy = _copy_double_pointer(env, lenv - 1, i);
+				if (copy == NULL)
+				{
+					handle_error(7, shell_info, 1);
+					return (NULL);
+				}
+			}
+			else
+			{
+				shell_info->unset_environment[0] = 1;
+				copy = NULL;
+			}
+
+			free_double_pointer(env);
+			return (copy);
+		}
 	}
-    }
+	if (found == 0)
+	{
+		write(2, "VARIABLE not found\n", 19);
+		return (NULL);
+	}
 
-    if (found == 0) {
-	write(2, "VARIABLE not found\n", 19);
-	return (NULL);
-    }
-
-    return (env);
+	return (env);
 }
