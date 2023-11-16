2bb3a092791db431a09e89e4f11d6f685d2c9d6d
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-16 21:48:52 +0100

    Update get_env.c

--- get_env.c
+++ get_env.c
@@ -7,37 +7,33 @@
  * @env: Array of environment variables
  * Return: Value of the environment variable or NULL if not found
  */
-char *_get_environment_variable(const char *name, char **env) {
-    size_t check;
-    size_t j;
-    size_t name_length;
-    int i;
+char *_get_environment_variable(const char *name, char **env)
+{
+	size_t check;
+	size_t j;
+	size_t name_length;
+	int i;
 
-    name_length = 0;
+	name_length = 0;
+	if (!env || !name)
+		return (NULL);
 
-    if (!env || !name)
-	return (NULL);
+	while (name[name_length] != '\0')
+		name_length++;
 
-    while (name[name_length] != '\0')
-	name_length++;
-
-    for (i = 0; env[i] != NULL; i++) {
-	check = 0;
-	j = 0;
-
-	/* Compare characters in the name with the current environment variable
-	 */
-	while (j < name_length && env[i][j] != '\0') {
-	    if (env[i][j] == name[j])
-		check++;
-	    j++;
+	for (i = 0; env[i] != NULL; i++)
+	{
+		check = 0;
+		j = 0;
+		while (j < name_length && env[i][j] != '\0')
+		{
+			if (env[i][j] == name[j])
+				check++;
+			j++;
+		}
+		if (check == name_length && env[i][check] == '=')
+			return (env[i]);
 	}
-
-	/* Check if the full name is matched, and the next character is '=' */
-	if (check == name_length && env[i][check] == '=')
-	    return (env[i]);
-    }
-
-    /* Return NULL if the environment variable is not found */
-    return (NULL);
+	/* Return NULL if the environment variable is not found */
+	return (NULL);
 }
