42b40b28ea62974c4939a246a598ac1ff9c1cc09
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-16 22:23:40 +0100

    Update str_conc.c

--- str_conc.c
+++ str_conc.c
@@ -8,35 +8,32 @@
  *
  * Return: Pointer to the concatenated string.
  */
-char *concatenate_strings(const char *s1, const char *s2) {
-    size_t l1, l2;
-    char *result;
+char *concatenate_strings(const char *s1, const char *s2)
+{
+	size_t l1, l2;
+	char *result;
 
-    if (s1 == NULL) {
-	s1 = "";
-    }
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
 
-    if (s2 == NULL) {
-	s2 = "";
-    }
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
-    /* Calculate the lengths of the strings */
-    l1 = string_length(s1);
-    l2 = string_length(s2);
+	l1 = string_length(s1);
+	l2 = string_length(s2);
+	result = malloc(sizeof(char) * (l1 + l2 + 1));
 
-    /* Allocate memory for the result string */
-    result = malloc(sizeof(char) * (l1 + l2 + 1));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
 
-    /* Check for allocation failure */
-    if (result == NULL) {
-	return (NULL);
-    }
-
-    /* Copy the first string into the result */
-    _string_copy(result, s1);
-
-    /* Copy the second string into the result after the first one */
-    _string_copy(result + l1, s2);
-
-    return result;
+	_string_copy(result, s1);
+	/* Copy the second string into the result after the first one */
+	_string_copy(result + l1, s2);
+	return result;
 }
