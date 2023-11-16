affcd0865a8c1eb13d07a2edade385da1bf30e76
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-16 22:21:15 +0100

    Update sig_handler.c

--- sig_handler.c
+++ sig_handler.c
@@ -5,8 +5,10 @@
  * handle_signal - Signal handler for handling ctrl + c during runtime
  *
  * @signal_number: Unused parameter (just for betty)
+ * Return: 0
  */
-void handle_signal(int signal_number) {
+void handle_signal(int signal_number)
+{
     (void)signal_number;
     write(1, "\n$ ", 3);
 }
@@ -16,8 +18,10 @@ void handle_signal(int signal_number) {
  * execution
  *
  * @signal_number: Unused parameter (just for betty)
+ * Return: 0
  */
-void handle_signal2(int signal_number) {
+void handle_signal2(int signal_number)
+{
     (void)signal_number;
     write(1, "\n", 1);
 }
