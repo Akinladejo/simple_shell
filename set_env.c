637199481af7fb3376d71dda336be02e529d2148
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-17 13:22:04 +0100

    Update set_env.c

--- set_env.c
+++ set_env.c
@@ -33,8 +33,10 @@ void free_double_pointer(char **pointer)
 char **copy_double_pointer(char **pointer, int old_size, int new_size)
 {
 	int i; /* Move declaration to the beginning */
-	int copy_size = (new_size < old_size) ? new_size : old_size;
-	char **copy = malloc(sizeof(char *) * (copy_size + 1));
+	int copy_size
+	char **copy
+	copy_size = (new_size < old_size) ? new_size : old_size;
+	copy = malloc(sizeof(char *) * (copy_size + 1));
 
 	if (!copy)
 		return NULL;
@@ -73,83 +75,4 @@ int string_array_length(char **array)
 		;
 
 	return length;
-}
-
-/**
- * set_environment_variable - Sets an environment variable.
- * @env: The environment array.
- * @variable: The variable to be set.
- * @value: The value to set for the variable.
- * @shell_info: The shell information.
- *
- * This function sets an environment variable.
- * Returns the modified environment array or NULL on failure.
- */
-char **set_environment_variable(char **env, char *variable, char *value,
-ShellInfo *shell_info)
-{
-	int len_variable;
-	int len_env;
-	char *env_join2;
-	int i;
-	char *env_join;
-	char **copy;
-	char *copy_dup;
-
-	if (!variable || string_length(variable) == 0)
-		return (handle_error(3, shell_info, 1), NULL);
-
-	len_variable = string_length(variable);
-	len_env = string_array_length(env);
-	env_join2 = concatenate_strings(variable, "=");
-
-	if (!env_join2)
-		return (handle_error(3, shell_info, 1), NULL);
-
-	env_join = concatenate_strings(env_join2, value);
-	free(env_join2);
-
-	if (!env_join)
-		return (handle_error(3, shell_info, 1), NULL);
-
-	for (i = 0; env && env[i] != NULL; i++)
-	{
-		int check = 0;
-		int j;
-		for (j = 0; j < len_variable && env[i][j] != '\0'; j++)
-		{
-			if (variable[j] == '=')
-				return (free(env_join), handle_error(3, shell_info, 2), NULL);
-
-			if (env[i][j] == variable[j])
-				check++;
-		}
-
-		if (check == len_variable && env[i][check] == '=')
-		{
-			free(env[i]);
-			copy_dup = _str_duplicate(env_join);
-			free(env_join);
-
-			if (!copy_dup)
-				return (handle_error(3, shell_info, 1), NULL);
-
-			return (env[i] = copy_dup, env);
-		}
-	}
-
-	copy = copy_double_pointer(env, len_env, len_env + 1);
-	free_double_pointer(env);
-
-	if (!copy)
-		return (free(env_join), handle_error(3, shell_info, 1), NULL);
-
-	env = copy;
-	copy_dup = _str_duplicate(env_join);
-	free(env_join);
-
-	if (!copy_dup)
-		return (handle_error(3, shell_info, 1), NULL);
-
-	return (env[len_env] = copy_dup, env);
 }
