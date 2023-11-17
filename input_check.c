062b1595c96c8dfed1fd06e21018661865122d0a
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-17 12:22:12 +0100

    Update input_check.c

--- input_check.c
+++ input_check.c
@@ -17,34 +17,42 @@
  *
  * Return: On success, returns a command array. On error, exits the program.
  */
-char **check_input(int argument_count, char **arguments, size_t *buffer_size,
-char **buffer, ShellInfo *shell_info)
-{
-	ssize_t characters; char **command; int exit_number;
 
+char **get_user_input(size_t *buffer_size, char **buffer, ShellInfo *shell_info)
+{
+	ssize_t characters;
+	char **command;
+	int exit_number;
+
+	if (isatty(STDIN_FILENO))
+		write(1, "$ ", 2);
+	characters = getline(buffer, buffer_size, stdin);
+	if (characters == -1)
+	{
+		exit_number = shell_info->exit_number[0];
+		free(*buffer);
+		if (*(shell_info->environment_copy))
+			free_double_pointer(*(shell_info->environment_copy));
+		free(shell_info);
+		if (isatty(STDIN_FILENO))
+			write(1, "\n", 1);
+		exit(exit_number);
+	}
+	if (**buffer == '#' || !characters || **buffer == '\n')
+		return (NULL);
+	*buffer = remove_comments(*buffer);
+	command = get_parameters(*buffer, shell_info);
+	return (command);
+}
+
+char **check_input(int argument_count, char **arguments, size_t *buffer_size,
+                   char **buffer, ShellInfo *shell_info)
+{
+	char **command;
 	switch (argument_count)
 	{
 		case 1:
-			if (isatty(STDIN_FILENO))
-				write(1, "$ ", 2);
-			characters = getline(buffer, buffer_size, stdin);
-			if (characters == -1)
-			{
-				exit_number = shell_info->exit_number[0];
-				free(*buffer);
-
-				if (*(shell_info->environment_copy))
-					free_double_pointer(*(shell_info->environment_copy));
-				free(shell_info);
-
-				if (isatty(STDIN_FILENO))
-					write(1, "\n", 1);
-				exit(exit_number);
-			}
-			if (**buffer == '#' || !characters || **buffer == '\n')
-				return (NULL);
-			*buffer = remove_comments(*buffer);
-			command = get_parameters(*buffer, shell_info);
+			command = get_user_input(buffer_size, buffer, shell_info);
 			break;
 		default:
 			command = malloc(sizeof(char *) * argument_count);
