e8ede5fcbdd61f4908e1dfa8aadbb83455203c6e
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-17 14:30:04 +0100

    Update unset_env.c

--- unset_env.c
+++ unset_env.c
@@ -5,7 +5,6 @@
 
 /**
  * _copy_double_pointer - copies an array of strings (double pointer)
- *
  * @p: double pointer to copy
  * @new_size: size of copy
  * @jump: value that must be skipped in copy
@@ -19,10 +18,8 @@ char **_copy_double_pointer(char **p, int new_size, int jump)
 
 	csize = new_size;
 	copy = malloc(sizeof(char *) * (csize + 1));
-
 	if (copy == NULL)
 		return (NULL);
-
 	for (i = 0, j = 0; j < csize; i++, j++)
 	{
 		if (i == jump)
@@ -31,22 +28,18 @@ char **_copy_double_pointer(char **p, int new_size, int jump)
 		if (copy[j] == NULL)
 		{
 			j--;
-
 			for (; j >= 0; j--)
 				free(copy[j]);
 			free(copy);
-
 			return (NULL);
 		}
 	}
-
 	copy[new_size] = NULL;
 	return (copy);
 }
 
 /**
  * _unset_environment_variable - unsets an environmental variable
- *
  * @env: array of env variables
  * @variable: env variable to unset
  * @shell_info: struct with shell info
@@ -60,22 +53,13 @@ ShellInfo *shell_info)
 	char **copy;
 
 	shell_info->unset_environment[0] = 0;
-
-	if (!env)
+	if (!env || string_length(variable) == 0 || variable == NULL)
 	{
-		handle_error(3, shell_info, 1);
+		handle_error(3, shell_info, (!env) ? 1 : 2);
 		return (NULL);
 	}
-
-	if (string_length(variable) == 0 || variable == NULL)
-	{
-		handle_error(3, shell_info, 2);
-		return (NULL);
-	}
-
 	l = string_length(variable);
 	lenv = string_array_length(env);
-
 	for (i = 0; env[i] != NULL; i++)
 	{
 		for (check = 0, j = 0; j < l && env[i][j] != '\0'; j++)
@@ -85,16 +69,13 @@ ShellInfo *shell_info)
 				handle_error(3, shell_info, 3);
 				return (NULL);
 			}
-
 			if (env[i][j] == variable[j])
 				check++;
 		}
-
 		if (check == l && env[i][check] == '=')
 		{
 			/* Found env to erase */
 			found = 1;
-
 			if ((lenv - 1) != 0)
 			{
 				copy = _copy_double_pointer(env, lenv - 1, i);
@@ -109,7 +90,6 @@ ShellInfo *shell_info)
 				shell_info->unset_environment[0] = 1;
 				copy = NULL;
 			}
-
 			free_double_pointer(env);
 			return (copy);
 		}
@@ -119,6 +99,5 @@ ShellInfo *shell_info)
 		write(2, "VARIABLE not found\n", 19);
 		return (NULL);
 	}
-
 	return (env);
 }
