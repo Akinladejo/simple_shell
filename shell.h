8addf7ff7826f833670a0b7e5d31ed04bab2145f
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-17 08:03:34 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -164,13 +164,6 @@ ssize_t execute_built_ins(ShellInfo *shell_info);
 char *auxiliary_cd(ShellInfo *shell_info, char *current_directory);
 char *auxiliary_cd_2(ShellInfo *shell_info, char *current_directory);
 ssize_t cd_command(ShellInfo *shell_info);
-void update_environment_variables(char *directory);
-void change_directory(char *directory);
-char *get_directory(ShellInfo *shell_info);
-char *get_current_directory(void);
-int check_num_args(ShellInfo *shell_info);
-int validate_cd_arguments(ShellInfo *shell_info);
-void cleanup(ShellInfo *shell_info, char *current_directory, char *directory);
 
 /**
  * exit_command - Handle the exit command
