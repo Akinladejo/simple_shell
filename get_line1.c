94960bd18ff751cb44df918aea969c2a4649c514
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-11-17 08:47:50 +0100

    get_line1.c

---
+++ get_line1.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * _memset - Fills the first n bytes of the memory area pointed to by dest
+ *           with the constant byte value.
+ *
+ * @dest: Pointer to the memory area
+ * @value: Byte value to be set
+ * @count: Number of bytes to set
+ * Return: (Pointer to the memory area dest)
+ */
+char *_memset(char *dest, char value, unsigned int count)
+{
+	int i;
+
+	for (unsigned i = 0; i < count; i++)
+	{
+		dest[i] = value;
+	}
+	return (dest);
+}
